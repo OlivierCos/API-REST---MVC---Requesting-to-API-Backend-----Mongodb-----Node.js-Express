0f33efe2865fb79d70f2f0d46a1afb0f4c7fc1e6
Olivier Costel <ocost@hotmail.com>
2021-09-13 13:45:34 +0200

    modif variables d'environnement fichier .env

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -33,7 +33,7 @@ exports.login = (req, res, next) => {
                   userId: user._id,
                   token: jwt.sign(
                     { userId: user._id },
-                    process.env.exemple.TOKEN,
+                    process.env.TOKEN,
                     { expiresIn: '24h' }
                   )
                 });
